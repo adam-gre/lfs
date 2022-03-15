8b66b4c255101649f85237afa36ce45ad37a96b1
Adam <agreenwood1305@gmail.com>
2022-03-15 11:53:55 +0000

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import faunadb from 'faunadb';
 const client = new faunadb.Client({
-  secret: "fnAEF0Lh_wACAX3zy5osqhfBhe7mwelrOoIKoiUf"
+  secret: "fnAEF0Lh_wACAX3zy5osqhfBhe7mwelrOoIKoiUf"  //Invalid
 });
 const q = faunadb.query;
 export { client, q };
